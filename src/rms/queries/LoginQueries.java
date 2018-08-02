6094f59ca90849a1cbee6446fc0ec044b6a615cd
Unknown <mbjudeh3@uno.edu>
2018-08-01 20:34:07 -0700

    Added login functionality for users.
    Methods that Returns a user based on either username and password or on email and password. Both can throw EmptyResultDataAccessException if there is not user or if the password is incorrect. This is intentional and must be handled on the front end.

---
+++ src/rms/queries/LoginQueries.java
@@ -0,0 +1,31 @@
+package rms.queries;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import rms.mapper.UsersMapper;
+import rms.model.Users;
+
+public class LoginQueries {
+	private ApplicationContext context;
+	private JdbcTemplate jtemp;
+	
+	public LoginQueries() {
+
+        this.context = new ClassPathXmlApplicationContext("spring-dao.xml");
+        this.jtemp = (JdbcTemplate)context.getBean("jt");
+	}
+	
+	public Users loginOnEmail(String email, String password) throws EmptyResultDataAccessException {
+		return jtemp.queryForObject("SELECT * FROM USERS WHERE USER_EMAIL = ? AND USER_PASSWORD = ?", 
+				new UsersMapper(), email, password);
+	}
+	
+	public Users loginOnUserName(String userName, String password) throws EmptyResultDataAccessException {
+		return jtemp.queryForObject("SELECT * FROM USERS WHERE user_name = ? AND user_password = ?", 
+				new UsersMapper(), userName, password);
+	}
+
+}
