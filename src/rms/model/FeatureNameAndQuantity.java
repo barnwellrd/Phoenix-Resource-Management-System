04338b166f9640b556a70cf2747ea8fc4ca7fba9
Aria <amini5454@gmail.com>
2018-08-02 17:42:29 -0700

    Added query to get feature name and quantity. Also added a FeatureNameAndQuantity class along with its mapper.

---
+++ src/rms/model/FeatureNameAndQuantity.java
@@ -0,0 +1,13 @@
+package rms.model;
+
+public class FeatureNameAndQuantity {
+	
+	public final String featureName;
+	public final int quantity;
+	
+	public FeatureNameAndQuantity(String featureName, int quantity) {
+		this.featureName = featureName;
+		this.quantity = quantity;
+	}
+	
+}
