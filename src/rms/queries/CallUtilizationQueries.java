aff6834a9958ce0e6c1b82d532ce9070fed32a8d
Unknown <mbjudeh3@uno.edu>
2018-08-03 15:55:18 -0700

    Added calls to the queries

--- src/rms/queries/CallUtilizationQueries.java
+++ src/rms/queries/CallUtilizationQueries.java
@@ -1,49 +1,84 @@
 package rms.queries;
 
-public class CallUtilizationQueries {
-	public Double callDailyUtilizationByResourceId(int resourceId, String date)
-    {
-        return null;
-		
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class CallUtilizationQueries {
+	public static void main(String[] args) {
+		
+	}
+	public Double callDailyUtilizationByResourceId(int resourceId, Date date)
+    {		
+        return new UtilizationQueries().dailyUtilizationByResourceId(resourceId, formatDate(date));		
     }
 	
-	public Double callWeeklyUtilizationByResourceId(int resourceId, String startDate)
+	public Double callWeeklyUtilizationByResourceId(int resourceId, Date startDate)
 	{
+        Date endDate = addWeek(startDate);
+		return new UtilizationQueries().weeklyUtilizationByResourceId(resourceId, formatDate(startDate), formatDate(endDate));
+	}
+	
+	public Double callMonthlyUtilizationByResourceId(int resourceId, Date startDate)
+	{
+		Date endDate = addMonth(startDate);
+		return new UtilizationQueries().monthlyUtilizationByResourceId(resourceId, formatDate(startDate), formatDate(endDate));
+	}
+	
+	public Double callDailyUtilizationByResourceTypeId(int resourceTypeId, Date date)
+	{
+		return new UtilizationQueries().dailyUtilizationByResourceTypeId(resourceTypeId, formatDate(date));	
+	}
+	
+	public Double callWeeklyUtilizationByResourceTypeId(int resourceTypeId, Date startDate)
+	{
+		
+		Date endDate = addWeek(startDate);
+		return new UtilizationQueries().weeklyUtilizationByResourceTypeId(resourceTypeId, formatDate(startDate), formatDate(endDate));
+	}
+	
+	public Double callMonthlyUtilizationByResourceTypeId(int resourceTypeId, Date startDate)
+	{
+		Date endDate = addMonth(startDate);
+		return new UtilizationQueries().monthlyUtilizationByResourceTypeId(resourceTypeId, formatDate(startDate), formatDate(endDate));
+	}
+	
+	public Double callDailyUtilizationForAllResources(Date date) {
+		
+		return new UtilizationQueries().dailyUtilizationForAllResources(formatDate(date));
+		
+	}
+	
+	public Double callWeeklyUtilizationForAllResources(Date startDate) {
+		Date endDate = addWeek(startDate);
+		return new UtilizationQueries().weeklyUtilizationForAllResources(formatDate(startDate), formatDate(endDate));
+	}
+	
+	public Double callMonthlyUtilizationForAllResources(Date startDate) {
+		Date endDate = addMonth(startDate);
+		return new UtilizationQueries().monthlyUtilizationForAllResources(formatDate(startDate), formatDate(endDate));
+	}
+	
+	public String formatDate(Date date) {
+		SimpleDateFormat ddmmyyyyy = new SimpleDateFormat("dd-MM-yyyy");
 
-		return null;
+		return ddmmyyyyy.format(date) +" 00:00";
 	}
 	
-	public Double callMonthlyUtilizationByResourceId(int resourceId, String startDate)
-	{
-		return null;
+	public Date addMonth(Date startDate) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(startDate);
+		calendar.add(Calendar.MONTH, 1);
+
+		return calendar.getTime();
 	}
 	
-	public Double callDailyUtilizationByResourceTypeId(int resourceTypeId, String date)
-	{
-		return null;	
-	}
-	
-	public Double callWeeklyUtilizationByResourceTypeId(int resourceTypeId, String startDate)
-	{
-		return null;
-	}
-	
-	public Double callMonthlyUtilizationByResourceTypeId(int resourceTypeId, String startDate)
-	{
-		return null;
-	}
-	
-	public Double callDailyUtilizationForAllResources(String date) {
-		return null;
-	}
-	
-	public Double callWeeklyUtilizationForAllResources(String startDate) {
-		return null;
-	}
-	
-	public Double callMonthlyUtilizationForAllResources(String startDate) {
-		return null;
+	public Date addWeek(Date startDate) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(startDate);
+		calendar.add(Calendar.DAY_OF_YEAR, 7);
+
+		return calendar.getTime();
 	}
 
 }
