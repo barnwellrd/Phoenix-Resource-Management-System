2d4099b85314baf210b9e0a1171711fde8362da8
Unknown <kevinmac727@gmail.com>
2018-08-07 10:29:26 -0700

    RoomService comment

--- src/rms/services/RoomService.java
+++ src/rms/services/RoomService.java
@@ -14,18 +14,28 @@ import rms.model.FeaturesDropDown;
 import rms.model.Features;
 import rms.queries.FeatureQueries;
 
+/**
+ * Service class to provide room details
+ * @author Re: Syntellions
+ *
+ */
 @Controller 
 public class RoomService 
 {
-	
+	/**
+	 * Service method called when RoomDetails page is reached.
+	 * A new FeaturesDropDown list is created to call the getFeatureNameAndQuantityByResouceId method from the FeatureQueries class.
+	 * The result is added to the ModelMap. 
+	 * @param req HttpServletRequest
+	 * @param res HttpServletResponse
+	 * @param map ModelMap used to store room information
+	 * @return The RoomDropDown.jsp view 
+	 */
 	@RequestMapping(value="/RoomDetails")
 	public String defaultService(HttpServletRequest req, HttpServletResponse res, ModelMap map)
 	{
-
 		List<FeaturesDropDown> listOfFeatures = new FeatureQueries().getFeatureNameAndQuantityByResouceId();
 		map.addAttribute("featData", listOfFeatures);
-		
-
 		
 		return "RoomDropDown";  //view name
 	}
